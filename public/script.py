a3c533115e7baf04a23faf333f18f43dced5060b
medeuamangeldi <60564887+medeuamangeldi@users.noreply.github.com>
2022-01-22 02:33:43 -0600

    nice

--- public/script.py
+++ public/script.py
@@ -1,11 +1,10 @@
 import sys
 import numpy as np
-sys.stdout.reconfigure(encoding='utf-8')
 
 def func(x):
     return float(sys.argv[1]) + (float(sys.argv[2])-float(sys.argv[1]))*((1+float(sys.argv[3])*x)**float(sys.argv[4]))**((float(sys.argv[5])-1)/float(sys.argv[4]))
 
-r = np.linspace(0, float(sys.argv[6]), 50)
+r = np.linspace(0, float(sys.argv[6]), 25)
 
 print(list(r), '%', list(func(r)))
 
