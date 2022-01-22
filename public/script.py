48c11827560eb6feaa720a0733e2babfc847fb6a
medeuamangeldi <60564887+medeuamangeldi@users.noreply.github.com>
2022-01-22 02:22:04 -0600

    nice

--- public/script.py
+++ public/script.py
@@ -1,7 +1,6 @@
 import sys
 import numpy as np
-# sys.stdout.reconfigure(encoding='utf-8')
-
+sys.stdout.reconfigure(encoding='utf-8')
 
 def func(x):
     return float(sys.argv[1]) + (float(sys.argv[2])-float(sys.argv[1]))*((1+float(sys.argv[3])*x)**float(sys.argv[4]))**((float(sys.argv[5])-1)/float(sys.argv[4]))
