d95ad8109aecae842f95027d02c4156bdaaa93fe
medeuamangeldi <60564887+medeuamangeldi@users.noreply.github.com>
2022-01-22 02:00:35 -0600

    added inputs and download button

--- index.js
+++ index.js
@@ -21,9 +21,38 @@ app.get("/apps", function(req, res){
 });
 
 app.get("/pump", function(req, res){
-    res.render("pages/pump");
+    res.render("pages/pump", {OutputLabel: JSON.stringify([5, 4, 2, 0.5]), OutputData: JSON.stringify([5, 4, 2, 0.5]), inputData: JSON.stringify([5, 4, 2, 0.5]), labelData: JSON.stringify([5, 4, 2, 0.5])});
 });
 
 app.get("/about", function(req, res){
     res.render("pages/about");
+});
+
+app.post("/run", function(req, res){
+    var dataToSend;
+    var input_data;
+    var label_data;
+
+    const a = req.body.a,
+          b = req.body.b,
+          c = req.body.c, 
+          d = req.body.d, 
+          e = req.body.e,
+          rad = req.body.rad; 
+
+    const python = spawn('python', ['public/script.py', a, b, c, d, e, rad]);
+    // req.body.tokensNumber, req.body.topP
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+        const arrays = dataToSend.split('%');
+        input_data = arrays[1];
+        label_data = arrays[0];
+       });
+
+    python.on('exit', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        res.render("pages/pump", {OutputLabel: label_data, OutputData: input_data, inputData: input_data, labelData: label_data});
+    });
+
 });
---
+++ public/script.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+sys.stdout.reconfigure(encoding='utf-8')
+
+
+def func(x):
+    return float(sys.argv[1]) + (float(sys.argv[2])-float(sys.argv[1]))*((1+float(sys.argv[3])*x)**float(sys.argv[4]))**((float(sys.argv[5])-1)/float(sys.argv[4]))
+
+r = np.linspace(0, float(sys.argv[6]), 50)
+
+print(list(r), '%', list(func(r)))
+
+sys.stdout.flush()
