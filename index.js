d2f94fc3bc7c41bf77413f462311d5e9ad6c3658
medeuamangeldi <60564887+medeuamangeldi@users.noreply.github.com>
2022-01-22 05:09:28 -0600

    with js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express"),
       app = express(),
       bodyParser = require('body-parser'),
-      {spawn} = require('child_process'),
       port = 3000;
+
+var nj = require('numjs');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +22,7 @@ app.get("/apps", function(req, res){
 });
 
 app.get("/pump", function(req, res){
-    res.render("pages/pump", {OutputLabel: JSON.stringify([5, 4, 2, 0.5]), OutputData: JSON.stringify([5, 4, 2, 0.5]), inputData: JSON.stringify([5, 4, 2, 0.5]), labelData: JSON.stringify([5, 4, 2, 0.5])});
+    res.render("pages/pump", {OutputLabel: JSON.stringify([5, 4, 2, 0.5]), OutputData: JSON.stringify([5, 4, 2, 0.5]), inputData: [5, 4, 2, 0.5], labelData: ['5', '4', '2', '0.5']});
 });
 
 app.get("/about", function(req, res){
@@ -29,23 +30,38 @@ app.get("/about", function(req, res){
 });
 
 app.post("/run", function(req, res){
-    var dataToSend;
     var input_data;
     var label_data;
-    
-    const python = spawn('python3', ['public/script.py', req.body.a, req.body.b, req.body.c, req.body.d, req.body.e, req.body.rad]);
-    // req.body.tokensNumber, req.body.topP
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-        const arrays = dataToSend.split('%');
-        input_data = arrays[1];
-        label_data = arrays[0];
-       });
 
-    python.on('exit', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        res.render("pages/pump", {OutputLabel: label_data, OutputData: input_data, inputData: input_data, labelData: label_data});
-    });
+    const a = req.body.a,
+          b = req.body.b, 
+          c = req.body.c, 
+          d = req.body.d, 
+          e = req.body.e, 
+          rad = req.body.rad;
+    
+    var Domain = nj.arange(0, rad, 0.5).tolist();
+
+    var someFunction = function(x) {
+      return a+(b-a)*(1+(c*x)**d)**((e-1)/d);  
+    };
+    
+    var makeStr = function(x) {
+        return x.toString();
+    };
+
+    var makeFloat = function(x) {
+        return parseFloat(x);
+    };
+
+    var Range = Domain.map(someFunction);
+
+    label_data = Domain.map(makeStr);
+    input_data = Range.map(makeFloat);
+
+    console.log(label_data);
+    console.log(input_data);
+
+    res.render("pages/pump", {OutputLabel: JSON.stringify(Domain), OutputData: JSON.stringify(input_data), inputData: input_data, labelData: label_data});
 
 });
